e5d38732b85ef252fffa5df1077456a161e08f14
Artem Z. <artemonych628@gmail.com>
2025-11-11 12:07:15 +0100

    feat: Added more routes - based on figma

--- frontend/menza-web/src/main.tsx
+++ frontend/menza-web/src/main.tsx
@@ -6,14 +6,22 @@ import "./index.css"
 import {BrowserRouter, Route, Routes} from "react-router";
 import Login from "./routes/Login.tsx";
 import Home from "./routes/Home.tsx";
+import MenuPage from "./routes/MenuPage.tsx";
+import HistoryPage from "./routes/HistoryPage.tsx";
+import MealDetailPage from "./routes/MealDetailPage.tsx";
+import CanteenOccupancyPage from "./routes/CanteenOccupancyPage.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<Login/>}/>
-                <Route element={<MainLayout/>}>
-                    <Route index path={"/"} element={<Home/>}/>
+                <Route path="/" element={<MainLayout/>}>
+                    <Route index element={<Home/>}/>
+                    <Route path="menu" element={<MenuPage/>}/>
+                    <Route path="history" element={<HistoryPage/>}/>
+                    <Route path="menu/:id" element={<MealDetailPage/>}/>
+                    <Route path="occupancy/:canteenName" element={<CanteenOccupancyPage/>}/>
                 </Route>
             </Routes>
         </BrowserRouter>
---
+++ frontend/menza-web/src/routes/MealDetailPage.tsx
@@ -0,0 +1,8 @@
+import {useParams} from "react-router";
+
+export default function MealDetailPage(){
+    const params = useParams();
+    const mealId = params.id;
+
+    return <div>FoodDetailPage for food with id {mealId}</div>
+}
