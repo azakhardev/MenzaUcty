08f58bdd5be43ef07ea822038dd07ba79428013f
Artem Z. <artemonych628@gmail.com>
2025-11-18 11:36:53 +0100

    feat: Link na Swagger

--- frontend/menza-web/src/components/layout/Header.tsx
+++ frontend/menza-web/src/components/layout/Header.tsx
@@ -10,10 +10,12 @@ export default function Header() {
 
     return <div className="w-full bg-gray-300 flex flex-col items-center justify-center">
         {/*<h1 className="text-2xl font-bold">WebCredit 2.0</h1>*/}
-        <div className="sticky top-0 w-full p-4 flex flex-row justify-center">
+        <div className="sticky top-0 w-full p-4 flex flex-row justify-between">
+            <div></div>
             <h2 className="text-xl font-bold">
                 {CANTEENS[canteen] ?? "Unknown"}
             </h2>
+            <a href="http://localhost:8080/swagger-ui/index.html" className="text-blue-700 underline" target="_blank" > Swagger </a>
         </div>
     </div>
 }
